77ca10592b71f38d48e7ef08f503f11a66b3ac29
ajaydatla <ajaydatlalbn06@gmail.com>
2018-11-18 22:49:09 +0530

    basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { id: 1, name: "Eminem", age: 45 },
+      { id: 2, name: "Steve Jobs", age: 60 },
+      { id: 3, name: "Heath Ledger", age: 38 }
+    ],
+    showNames: false
+  }
+
+  switchNameHandler = (newName) => {
+    console.log('he ');
+    this.setState({
+      persons: [
+        { name: newName, age: 45 },
+        { name: "Steve Jobs", age: 60 },
+        { name: "Heath Ledger", age: 38 }
+      ]
+    })
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Eminem", age: 45 },
+        { name: event.target.value, age: 60 },
+        { name: "Heath Ledger", age: 38 }
+      ]
+    })
+  }
+
+  toggerPersonHandler = () => {
+    const doesShow = this.state.showNames;
+    this.setState({
+      showNames: !doesShow
+    });
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let person = null;
+
+    if (this.state.showNames) {
+      person = (<div>
+        {this.state.persons.map(singlePerson => {
+          return <Person name={singlePerson.name} age={singlePerson.age} key={singlePerson.id}/>;
+        })}
+
+      </div>);
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>This is survival of the real.</h1>
+        <button style={style} onClick={this.toggerPersonHandler}>Switch Name</button>
+        {person}
       </div>
     );
   }
